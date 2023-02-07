b01ebc1024f5ca7c7c1294f85b5b27ef88f89f12
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-07 09:46:26 +0100

    Update casino.sol

--- contracts/casino.sol
+++ contracts/casino.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.8.0;
 
 contract Casino {
     struct ProposedBet{
+        address sideA; //Address proposing the bet
+        uint value; // Valur Proposed
+        uint placedAt; // Timestamp 
+        bool accepted; // Accepted Boolean
     }
 
     struct AcceptedBet{
