cf87864d614a62a717a578c9c46c650b9588b333
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-08 13:56:38 +0100

    Update casino.js

--- test/casino.js
+++ test/casino.js
@@ -4,5 +4,6 @@ const { ethers } = require('hardhat')
 describe('Casino', async function() { 
     const[owner, otherAccount] = await ethers.getSigners()
     const Casino = await ethers.getContractFactory("Casino")
+    const casino = await Casino.deploy()
   
 })
