92030ad51ab7eed0e2e256986ab6f2486a354c16
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-08 09:54:36 +0100

    Update casino.sol

--- contracts/casino.sol
+++ contracts/casino.sol
@@ -60,6 +60,9 @@ contract Casino {
         address payable sideB = payable(acceptedBet[_commitment].sideB);
         uint _agreedRandom = random^acceptedBet[_commitment].randomB;
         uint value = proposedBet[_commitment].value;
+        require (proposedBet[_commitment].sideA == msg.sender);
+        require (proposedBet[_commitment].accepted);
+        
     }
 
     
