8bac5b6b2338c5fbb32e727b642f51fd72579fac
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-09 23:31:15 +0100

    Update casino.js

--- test/casino.js
+++ test/casino.js
@@ -4,7 +4,6 @@ const valBwin = ethers.utils.keccak256(0x600D60A7)
 
 describe('Casino', async function() { 
     it('Not allow you place a zero bet', async () => {
-    const[owner, otherAccount] = await ethers.getSigners()
     const Casino = await ethers.getContractFactory("Casino")
     const casino = await Casino.deploy()
     try{
