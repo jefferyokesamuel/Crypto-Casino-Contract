cc67c5b182061a5f0280478af21aa2bc92397ca8
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-09 23:31:12 +0100

    Update casino.js

--- test/casino.js
+++ test/casino.js
@@ -1,5 +1,6 @@
 const { ethers } = require('hardhat')
 
+const valBwin = ethers.utils.keccak256(0x600D60A7)
 
 describe('Casino', async function() { 
     it('Not allow you place a zero bet', async () => {
