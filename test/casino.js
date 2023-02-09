b7c8f4e9cc685bd4a99c8f31bfbb73d3ee818e87
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-09 23:31:09 +0100

    Update casino.js

--- test/casino.js
+++ test/casino.js
@@ -6,5 +6,8 @@ describe('Casino', async function() {
     const[owner, otherAccount] = await ethers.getSigners()
     const Casino = await ethers.getContractFactory("Casino")
     const casino = await Casino.deploy()
+    try{
+        tx = casio.proposeBet(hashA)
+    }
 });
 })
